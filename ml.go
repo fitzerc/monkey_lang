d81f917ae74f680d2fada89b7bc0badfbdb75853
chris <fitzer.c@gmail.com>
2023-12-19 15:14:59 -0600

    repl 1-5

--- ml.go
+++ ml.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"monkey_lang/repl"
+	"os"
+	"os/user"
+)
 
 func main() {
-	fmt.Println("sup")
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s. This is the Monkey programming language\n", user.Username)
+	fmt.Println("Try Some Commands")
+
+	repl.Start(os.Stdin, os.Stdout)
 }
